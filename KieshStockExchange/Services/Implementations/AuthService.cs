66789200d562a06dc294e8a1bd3dbc103c23decc
Kishan den Hartog <kjdenhartog@outlook.com>
2025-09-17 12:18:30 +0200

    changed User? CurrentUser to to an empty user and added a logger.

--- KieshStockExchange/Services/Implementations/AuthService.cs
+++ KieshStockExchange/Services/Implementations/AuthService.cs
@@ -1,6 +1,8 @@
-﻿using KieshStockExchange.Models;
+﻿using ExcelDataReader.Log;
 using KieshStockExchange.Helpers;
+using KieshStockExchange.Models;
 using KieshStockExchange.Services;
+using Microsoft.Extensions.Logging;
 
 namespace KieshStockExchange.Services.Implementations;
 
@@ -8,19 +10,30 @@ public class AuthService : IAuthService
 {
     
     private readonly IDataBaseService _db;
-    public User? CurrentUser { get; private set; }
-    public bool IsLoggedIn => CurrentUser != null;
-    public bool IsAdmin => CurrentUser?.IsAdmin ?? false;
+    private readonly ILogger<AuthService> _logger;
 
-    public AuthService(IDataBaseService db)
+    public User CurrentUser { get; private set; } = new();
+    public bool IsLoggedIn => CurrentUser.UserId != 0;
+    public bool IsAdmin => CurrentUser.IsAdmin;
+
+    public AuthService(IDataBaseService db, ILogger<AuthService> logger)
     {
         _db = db;
+        _logger = logger;
     }
 
     public async Task<bool> RegisterAsync(
         string username, string fullname,
         string email, string password, DateTime birthdate
     ) {
+        // Check for existing user
+        if (await _db.GetUserByUsername(username) != null)
+            return false;
+        if (await _db.GetUsersAsync().ContinueWith(t =>
+             t.Result.Any(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase)))) 
+            return false;
+
+        // Create user
         var user = new User
         {
             Username = username,
@@ -29,9 +42,6 @@ public class AuthService : IAuthService
             PasswordHash = SecurityHelper.HashPassword(password),
             BirthDate = birthdate
         };
-        // Check for existing user
-        if (await _db.GetUserByUsername(user.Username) != null)
-            return false;
 
         // Check validity
         if (!user.IsValid()) 
@@ -39,6 +49,10 @@ public class AuthService : IAuthService
 
         // Save user
         await _db.CreateUser(user);
+
+        _logger.LogInformation("New user registered: #{UserId} {Username}", user.UserId, user.Username);
+
+        //CurrentUser = user;
         return true;
     }
 
@@ -57,6 +71,6 @@ public class AuthService : IAuthService
 
     public async Task LogoutAsync()
     {
-        CurrentUser = null;
+        CurrentUser = new User();
     }
 }
