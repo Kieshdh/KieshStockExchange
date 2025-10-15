4c178b2e7a85a2a1ffd8b475c9cab3c49484dfd6
Kieshdh <kjdhartog@gmail.com>
2025-10-15 12:36:38 +0200

    Made initial version of the AIUser, will improve later

--- KieshStockExchange/Models/AIUser.cs
+++ KieshStockExchange/Models/AIUser.cs
@@ -6,18 +6,38 @@ using System.Threading.Tasks;
 
 namespace KieshStockExchange.Models;
 
-public class AIUser : User
+public class AIUser : IValidatable
 {
-    public int MinAmountStocks { get; set; } = 0;
-    public int MaxAmountStocks { get; set; } = 0;
-    public decimal OnlinePercentage { get; set; } = 0m;
-    public decimal Aggressiveness { get; set; } = 0m;
-    public decimal Balance { get; set; } = 0m;
-    public int[] WatchlistStocks { get; set; } = Array.Empty<int>();
+    public int AiUserId { get; init; }
+    public int Seed { get; init; }
+    public TimeSpan DecisionInterval { get; init; } = TimeSpan.FromSeconds(1);
 
+    #region Percentage Properties
+    public double TradeProb { get; init; } = 0.1;
+    public double UseMarketProb { get; init; } = 0.3;
+    public double OnlinePrc { get; set; } = 0;
+    public double MinTradeAmountPrc { get; set; } = 0;
+    public double MaxTradeAmountPrc { get; set; } = 0;
+    #endregion
 
-    public bool AiIsValid() => 
-        UserId > 0 && MinAmountStocks >= 0 && MaxAmountStocks >= MinAmountStocks &&
-        OnlinePercentage >= 0 && OnlinePercentage <= 1 && Aggressiveness >= 0 && 
-        Aggressiveness <= 1 && Balance >= 0 && WatchlistStocks != null && IsValid();
+    #region Trading Strategy Properties
+    public HashSet<int> WatchlistStocks { get; set; } = new();
+    public int MinOpenPositions { get; set; } = 0;
+    public int MaxOpenPositions { get; set; } = 0;
+    #endregion
+
+    #region IValidatable Implementation
+    public bool IsValid() => Seed > 0 && DecisionInterval.TotalSeconds >= 1 &&
+        IsValidPercentages() && IsValidMinMax() && IsValidStocks();
+
+    private bool IsValidPrc(double value) => value >= 0 && value <= 1;
+
+    private bool IsValidPercentages() => IsValidPrc(TradeProb) && IsValidPrc(UseMarketProb) &&
+        IsValidPrc(OnlinePrc) && IsValidPrc(MinTradeAmountPrc) && IsValidPrc(MaxTradeAmountPrc);
+
+    private bool IsValidMinMax() => MinOpenPositions >= 0 && MaxOpenPositions >= MinOpenPositions && MaxOpenPositions > 0 &&
+        MinTradeAmountPrc >= 0 && MaxTradeAmountPrc >= MinTradeAmountPrc && MaxTradeAmountPrc > 0;
+
+    private bool IsValidStocks() => WatchlistStocks.All(id => id > 0) && WatchlistStocks.Count == MaxOpenPositions;
+    #endregion
 }
