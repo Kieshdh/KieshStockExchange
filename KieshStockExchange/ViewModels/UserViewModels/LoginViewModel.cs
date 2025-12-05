d89f775daf436f97e0b1d5dbed18baa486b8aeb1
Kieshdh <kjdhartog@gmail.com>
2025-12-06 00:31:47 +0100

    Added regions and removed now unnecessary getRandomUser method

--- KieshStockExchange/ViewModels/UserViewModels/LoginViewModel.cs
+++ KieshStockExchange/ViewModels/UserViewModels/LoginViewModel.cs
@@ -4,18 +4,21 @@ using KieshStockExchange.Helpers;
 using System.Windows.Input;
 using KieshStockExchange.ViewModels.OtherViewModels;
 using KieshStockExchange.Services;
-using System.Diagnostics;
+using KieshStockExchange.Models;
 
 namespace KieshStockExchange.ViewModels.UserViewModels;
 
 public partial class LoginViewModel : BaseViewModel
 {
-    private readonly IAuthService _auth;
-
+    #region Properties
     [ObservableProperty] private string _username = String.Empty;
     [ObservableProperty] private string _password = String.Empty;
 
     public IAsyncRelayCommand LoginCommand { get; }
+    #endregion
+
+    #region Fields & Constructor
+    private readonly IAuthService _auth;
 
     public LoginViewModel(IAuthService auth)
     {
@@ -23,11 +26,13 @@ public partial class LoginViewModel : BaseViewModel
         _auth = auth ?? throw new ArgumentNullException(nameof(auth));
         LoginCommand = new AsyncRelayCommand(ExecuteLogin);
     }
+    #endregion
 
+    #region Login Methods and helpers
     public async Task AutoLogin() 
     {
         // Login with a random user each time
-        Username = "admin";   // GetRandomUser();
+        Username = "admin"; 
         Password = "hallo123";
 
         // Add default admin user if not exists
@@ -38,11 +43,12 @@ public partial class LoginViewModel : BaseViewModel
 
     private async Task ExecuteLogin()
     {
+        // Perform login
         await _auth.LoginAsync(Username, Password);
 
         if (_auth.IsLoggedIn)
             await Shell.Current.GoToAsync("//TradePage");
-            //await Shell.Current.GoToAsync("//AdminPage");
+        //await Shell.Current.GoToAsync("//AdminPage");
         else
             await Shell.Current.DisplayAlert("Error", "User and password combination does not exist", "OK");
     }
@@ -52,16 +58,5 @@ public partial class LoginViewModel : BaseViewModel
         await _auth.RegisterAsync("admin", "Admin User",
             "admin@gmail.com", "hallo123", DateTime.Parse("17-1-2000"));
     }
-
-    private string GetRandomUser()
-    {
-        string[] users = {
-            "flopez", "jesuslee", "hoganmichael", "lross",
-            "claudiaowens", "vfinley", "phamsandra",
-            "dakota11", "milleradrienne", "kingandrew",
-        };
-
-        int index = Random.Shared.Next(0, users.Length);
-        return users[index];
-    }
+    #endregion
 }
