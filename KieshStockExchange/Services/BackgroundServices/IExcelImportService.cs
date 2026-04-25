7c7d5a138da8c17d8d457cc16fc6633f63907c19
Kieshdh <kjdhartog@gmail.com>
2026-04-25 03:00:40 +0200

    Made the importservice a separate file

---
+++ KieshStockExchange/Services/BackgroundServices/IExcelImportService.cs
@@ -0,0 +1,13 @@
+namespace KieshStockExchange.Services.BackgroundServices;
+
+/// <summary>
+/// Defines methods to import data from Excel into the local database.
+/// </summary>
+public interface IExcelImportService
+{
+    /// <summary> Resets existing database entries and adds new databases from the Excel file. </summary>
+    Task ResetAndAddDatabases();
+
+    /// <summary> Checks for missing databases and adds them from the Excel file. </summary>
+    Task CheckAndAddDatabases();
+}
